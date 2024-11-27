e229028e8b0cc295f900cf5d53ed664e6dfaee09
selenkunkcu <selenkunkcu@gmail.com>
2024-11-27 22:04:01 +0300

    Skills page added

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -10,11 +10,10 @@ const Navbar = () => {
 
         </div>
         <h1 className="text-white text-[25px] font-semibold">
-          Webchain{" "}
           <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-red-500">
-            {" "}
-            Dev{" "}
+            selen
           </span>
+          kunkcu
         </h1>
       </div>
 
